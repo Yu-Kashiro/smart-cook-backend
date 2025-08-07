8d69a9c609837b6a831dad9dc21216d5385c3a6e
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-08-07 16:13:43 +0900

    Userモデルのメソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,33 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: self
+         :recoverable, :rememberable, :validatable
 
   def jwt_payload
-    super
+    { sub: id, exp: 24.hours.from_now.to_i }
+  end
+
+  def update_jti
+    self.jti = SecureRandom.uuid if has_attribute?(:jti)
+    save
+  end
+
+  # JWT revocation strategy methods
+  def self.jwt_revoked?(payload, user)
+    user.jti != payload['jti'] if user&.respond_to?(:jti)
+  end
+
+  def self.revoke_jwt(payload, user)
+    user&.update_jti if user&.respond_to?(:update_jti)
+  end
+
+  # Manual confirmation methods for API
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def confirm!
+    self.confirmed_at = Time.current
+    save(validate: false)
   end
 end

968520819a2ddb3ec28bb187486341f2c7c10b6e
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-08-07 16:41:41 +0900

    API関係共通のコントローラーを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
+
+  private
+
+  def authenticate_user!
+    token = request.headers['Authorization']&.split(' ')&.last
+
+    if token.present?
+      begin
+        decoded_token = JWT.decode(token, jwt_secret, true, { algorithm: 'HS256' })
+        @current_user = User.find(decoded_token[0]['sub'])
+      rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+        render json: { success: false, message: '認証に失敗しました' }, status: :unauthorized
+      end
+    else
+      render json: { success: false, message: '認証情報が必要です' }, status: :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user
+  end
+
+  def jwt_secret
+    ENV['DEVISE_JWT_SECRET_KEY'] || Rails.application.secret_key_base
+  end
 end
